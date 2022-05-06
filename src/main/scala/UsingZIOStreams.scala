3449fd06f0fe44853e962412499bee901a79ecbd
Miguel Lemos <miguellemos95@gmail.com>
2022-05-06 16:47:54 +0200

    Add simple model

--- src/main/scala/UsingZIOStreams.scala
+++ src/main/scala/UsingZIOStreams.scala
@@ -3,6 +3,14 @@ import zio._
 
 object UsingZIOStreams extends ZIOAppDefault {
 
+  // simple model
+  case class ProductId(id: String)
+  case class Product(id: ProductId, createdAt: java.time.YearMonth)
+  case class ItemId(id: String)
+  case class Item(id: ItemId, productId: ProductId)
+  case class OrderId(id: String)
+  case class Order(id: OrderId, items: Seq[(ItemId, Item)])
+
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
     ZIO.never
   }
