13e8752e470d0b490b526d6d766d1966f81454c1
Miguel Lemos <miguellemos95@gmail.com>
2022-05-06 16:49:04 +0200

    Add Generator companion object. We will use sala.util.Random to generate the dataset.

--- src/main/scala/UsingZIOStreams.scala
+++ src/main/scala/UsingZIOStreams.scala
@@ -13,7 +13,12 @@ object UsingZIOStreams extends ZIOAppDefault {
 
   // generator
   sealed trait Generator[A] { def next: A }
-
+  object Generator {
+    val random = {
+      val r = new scala.util.Random();
+      r // r.setSeed(0); r
+    }
+  }
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
     ZIO.never
   }
